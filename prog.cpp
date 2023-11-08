21084ef18df60b50fd7d51a24746badb940bfa97
Simon LEONARD <git-1001af4@sinux.sh>
2023-11-08 15:46:37 +0100

    feat: gray scale

--- prog.cpp
+++ prog.cpp
@@ -1,3 +1,4 @@
+#include <opencv2/imgcodecs.hpp>
 #include <iostream>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
@@ -9,11 +10,7 @@ int main( int argc, char** argv )
   int value = 128;
   namedWindow( "TP1");               // crée une fenêtre
   createTrackbar( "track", "TP1", &value, 255, NULL); // un slider
-  // createTrackbar( "track", "TP1", nullptr, 255, nullptr);
-  // setTrackbarPos( "track", "TP1", value );
-  Mat f = imread(argv[1]);        // lit l'image "lena.png"
-  // Si l'image n'est pas en niveau de gris, on la convertit
-  if (f.type() == CV_8UC3) cvtColor( f, f, COLOR_BGR2GRAY );
+  Mat f = imread(argv[1], IMREAD_GRAYSCALE);        // lit l'image
   imshow( "TP1", f );                // l'affiche dans la fenêtre
   while ( waitKey(50) < 0 )          // attend une touche
   { // Affiche la valeur du slider
