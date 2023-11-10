3cc90b95eb1ebd563246ca615ebfdd36eac7b3a1
Simon LEONARD <git-1001af4@sinux.sh>
2023-11-10 13:29:57 +0100

    fonctionne avec vscode

--- prog.cpp
+++ prog.cpp
@@ -1,24 +1,31 @@
-#include <opencv2/imgcodecs.hpp>
 #include <iostream>
 #include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 using namespace cv;
- 
-int main( int argc, char** argv ) 
-{
-  int old_value = 0;
-  int value = 128;
-  namedWindow( "TP1");               // crée une fenêtre
-  createTrackbar( "track", "TP1", &value, 255, NULL); // un slider
-  Mat f = imread(argv[1], IMREAD_GRAYSCALE);        // lit l'image
-  imshow( "TP1", f );                // l'affiche dans la fenêtre
-  while ( waitKey(50) < 0 )          // attend une touche
-  { // Affiche la valeur du slider
-    if ( value != old_value )
-    {
-      old_value = value;
-      // int new_value = getTrackbarPos( "track", "TP1" );
-      std::cout << "value=" << value << std::endl;
+
+int main(int argc, char **argv) {
+  namedWindow("TP1");                        // crée une fenêtre
+  Mat f = imread(argv[1], IMREAD_GRAYSCALE); // lit l'image
+  imshow("TP1", f);                          // l'affiche dans la fenêtre
+  // affiche l'histogramme
+  while (waitKey(50) < 0) {}
+}
+
+// Fonction qui prend une image et renvoie un tableau représentatn un histogramme
+double* histo(Mat f) {
+  double *hist = new double[256];
+  int nbPixel = f.rows * f.cols;
+  for (int i = 0; i < 256; i++) {
+    hist[i] = 0;
+  }
+  for (int i = 0; i < f.rows; i++) {
+    for (int j = 0; j < f.cols; j++) {
+      hist[f.at<uchar>(i, j)] = 1.0 / nbPixel;
     }
   }
-} 
+  return hist;
+}
+
+
+
